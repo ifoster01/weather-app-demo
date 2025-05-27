5a2699433a24a235897411b02e0567b9649ecf6d
Isaac Foster <ifoster41901@gmail.com>
2025-05-27 19:04:10 -0400

    feat: added microsoft clairy for user interaction tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/general/navbar';
 import QueryClientContext from './QueryClientProvider';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, 'clarity', 'script', 'rqc5pr9cxr');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <main className="min-h-screen bg-background">
           <QueryClientContext>
